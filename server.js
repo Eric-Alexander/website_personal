1b80d41eff4a01abbb0e2643bd0050dd1b0361fd
EricAlexanderG <EricAlexanderG@gmail.com>
2016-07-15 19:41:52 -0700

    hooked up server

---
+++ server.js
@@ -0,0 +1,26 @@
+// Require the Express Module
+var express = require('express');
+// Create an Express App
+var app = express();
+// Require body-parser (to receive post data from clients)
+var bodyParser = require('body-parser');
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+// Require path
+var path = require('path');
+// Setting Static Folder Directory
+app.use(express.static(path.join(__dirname, './static')));
+// Setting View Engine set to html
+app.set('view engine', 'html');
+// Routes
+// Root Request
+app.get('/', function(req, res) {
+
+    res.render('index');
+})
+
+
+// Setting Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
